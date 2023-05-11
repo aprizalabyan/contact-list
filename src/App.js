71f6cb5d0d4d22f64143cd5386b8f6ab1cf3ab2f
aprizalabyan <aprizalabyan@gmail.com>
2023-05-11 13:13:23 +0800

    Styling & responsive

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { useState } from 'react';
+import { Modal } from 'bootstrap/dist/js/bootstrap'
+import { uid } from 'uid';
+import { Icon } from '@iconify/react';
 import './App.css';
 import List from './List';
-import { uid } from 'uid';
 
 const App = () => {
   const [contacts, setContacts] = useState([
@@ -41,13 +43,13 @@ const App = () => {
   const handleSubmit = (e) => {
     e.preventDefault()
     let data = [...contacts] //copy semua data di State contacts
-
-    if(formData.name === ""){
-      alert("Nama Tidak Boleh Kosong")
-      return false
-    }
-    if(formData.telp === ""){
-      alert("Telpon Tidak Boleh Kosong")
+    const warningModal = new Modal(document.getElementById('warning-modal'))
+    const successModal = new Modal(document.getElementById('success-modal'))
+    const updateModal = new Modal(document.getElementById('update-modal'))
+    
+    if(formData.name === "" || formData.telp === ""){
+      warningModal.show()
+      setTimeout(() => { warningModal.hide() }, 3000)
       return false
     }
 
@@ -58,10 +60,12 @@ const App = () => {
           contact.telp = formData.telp
         }
       })
-      alert("Berhasil Edit Contact")
+      updateModal.show()
+      setTimeout(() => { updateModal.hide() }, 3000)
     } else {
       data.push({id: uid(), name: formData.name, telp: formData.telp}) //masukkan semua data dari input ke State contacts
-      alert("Berhasil Menambah Contact")
+      successModal.show()
+      setTimeout(() => { successModal.hide() }, 3000)
     }
 
     setIsEdit({id: null, status: false})
@@ -81,11 +85,11 @@ const App = () => {
     let filterData = data.filter((contact) => contact.id !== idDelete)  //menampilkan data contact kecuali id yang di delete
     setContacts(filterData)
   }
-
+  
   return (
     <div className="container">
-      <div className="row justify-content-center py-4">
-        <div className="col-4">
+      <div className="row justify-content-center pt-4">
+        <div className="col-10 col-sm-8 col-md-6 col-lg-5 col-xl-4">
           <h2 className="text-center mb-4">My Contact</h2>
           <h5 className="pt-4">Add Contact</h5>
           <form onSubmit={handleSubmit}>
@@ -100,6 +104,43 @@ const App = () => {
             <button type="submit" className="btn btn-primary w-100 mt-2">Save</button>
           </form>
           <List dataContact={contacts} handleEdit={handleEdit} handleDelete={handleDelete} />
+          <div className="bg-dark text-center mt-5 px-3 py-2">
+            <div className="smallText text-light">Made with react & bootstrap.
+              Uses fake json API to CRUD the data which uploaded on&nbsp;
+              <a href="https://github.com/aprizalabyan/fakeAPI" target="_blank" rel="noreferrer">github</a>.
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="modal fade" id="warning-modal" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div className="modal-dialog modal-sm modal-dialog-centered">
+          <div className="modal-content">
+            <div className="modal-body d-flex gap-2 align-items-center">
+              <h1><Icon icon="mdi:warning-circle-outline" /></h1>
+              <span>Name or Telp cannot be blank,<br/>
+              Please check again</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="modal fade" id="success-modal" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div className="modal-dialog modal-sm modal-dialog-centered">
+          <div className="modal-content">
+            <div className="modal-body d-flex flex-column align-items-center">
+              <h1><Icon icon="mdi:success-circle-outline" /></h1>
+              <span>Contact added successfully</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="modal fade" id="update-modal" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div className="modal-dialog modal-sm modal-dialog-centered">
+          <div className="modal-content">
+            <div className="modal-body d-flex flex-column align-items-center">
+              <h1><Icon icon="mdi:success-circle-outline" /></h1>
+              <span>Contact updated successfully</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/List.js
+++ src/List.js
@@ -14,7 +14,21 @@ const List = ({dataContact, handleEdit, handleDelete}) => {
               </div>
               <div className="d-flex gap-2 align-self-end">
                 <button className="btn btn-dark" onClick={() => handleEdit(contact.id)}>Edit</button>
-                <button className="btn btn-danger" onClick={() => handleDelete(contact.id)}>Delete</button>
+                <button className="btn btn-danger" data-bs-toggle="modal" data-bs-target={"#delete-modal-"+contact.id}>Delete</button>
+              </div>
+              <div className="modal fade" id={"delete-modal-"+contact.id} data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div className="modal-dialog modal-sm modal-dialog-centered">
+                  <div className="modal-content">
+                    <div className="modal-body">
+                      <span>Are you sure,<br/>
+                      You want to delete contact?</span>
+                    </div>
+                    <div className="modal-footer">
+                      <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                      <button type="button" className="btn btn-danger" onClick={() => handleDelete(contact.id)} data-bs-dismiss="modal">Delete</button>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           )
